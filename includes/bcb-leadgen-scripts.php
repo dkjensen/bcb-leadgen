f22f0899b361574d6623636b8d438fadf0dba57f
David Jensen <david@dkjensen.com>
2017-12-04 19:48:25 -0800

    Load GF scripts on leadpages

--- includes/bcb-leadgen-scripts.php
+++ includes/bcb-leadgen-scripts.php
@@ -8,3 +8,11 @@ function bcb_leadgen_scripts() {
     wp_enqueue_style( 'bcb-leadgen', BCB_LEADGEN_URL . '/assets/css/bcb-leadgen.css', array(), BCB_LEADGEN_VER );
 }
 add_action( 'wp_enqueue_scripts', 'bcb_leadgen_scripts', 15 );
+
+
+function bcb_leadgen_gf_scripts() {
+    if( is_singular( 'leadpage' ) && function_exists( 'gravity_form_enqueue_scripts' ) ) {
+        gravity_form_enqueue_scripts( (int) get_post_meta( get_queried_object_id(), 'leadpage_form_id', true ), true );
+    }
+}
+add_action( 'wp_head', 'bcb_leadgen_gf_scripts', 0 );
