1b342bff70bf4dae244798252330407d3e7dda35
David Jensen <david@dkjensen.com>
2017-12-06 12:24:01 -0800

    Remove extra text bug in admin menu

--- includes/bcb-leadgen-hooks.php
+++ includes/bcb-leadgen-hooks.php
@@ -61,32 +61,3 @@ function bcb_leadgen_create_form( $post_id, $post, $update ) {
     }
 }
 add_action( 'save_post_leadpage', 'bcb_leadgen_create_form', 10, 3 );
-
-
-function mbe_set_current_menu( $parent_file ) {
-    global $submenu_file, $current_screen, $pagenow;
-
-    //print_r( $current_screen );
-
-    print $parent_file;
-
-    # Set the submenu as active/current while anywhere in your Custom Post Type (nwcm_news)
-    if ( $current_screen->post_type == 'nwcm_news' ) {
-
-        if ( $pagenow == 'post.php' ) {
-            $submenu_file = 'edit.php?post_type=' . $current_screen->post_type;
-        }
-
-        if ( $pagenow == 'edit-tags.php' ) {
-            $submenu_file = 'edit-tags.php?taxonomy=nwcm_news_category&post_type=' . $current_screen->post_type;
-        }
-
-        $parent_file = 'nwcm';
-
-    }
-
-    return $parent_file;
-
-}
-
-add_filter( 'parent_file', 'mbe_set_current_menu' );
