ff2d867c08880242e0973fefa4f2dcf372f3e963
David Jensen <david@dkjensen.com>
2017-12-11 06:16:47 -0800

    Fix permalinks bug, update lead_cat and leadpage permalink structure

--- includes/bcb-leadgen-post-types.php
+++ includes/bcb-leadgen-post-types.php
@@ -5,6 +5,8 @@ if( ! defined( 'ABSPATH' ) )
 
 
 function bcb_leadgen_post_types() {
+    global $wp_rewrite;
+
     register_taxonomy( 'lead_cat', array( 'leadpage' ), array(
         'hierarchical'      => true,
 		'labels'            => array(
@@ -24,7 +26,7 @@ function bcb_leadgen_post_types() {
         'show_in_menu'      => false,
 		'show_admin_column' => true,
 		'query_var'         => true,
-		'rewrite'           => array( 'slug' => 'lead-cat' ),
+		'rewrite'           => array( 'slug' => 'campaign' ),
     ) );
 
 	register_post_type( 'leadpage', array(
@@ -54,12 +56,52 @@ function bcb_leadgen_post_types() {
 		'has_archive'        => true,
 		'hierarchical'       => false,
         'menu_position'      => null,
-        'rewrite'            => array(
-            'with_front'        => false,
-            'slug'              => '%lead_cat%'
-        ),
+        'rewrite'            => false,
         'menu_icon'          => 'dashicons-chart-line',
 		'supports'           => array( 'title', 'editor', 'author' )
     ) );
+    
+    $wp_rewrite->extra_permastructs['lead_cat']['struct'] = '%lead_cat%';
 }
-add_action( 'init', 'bcb_leadgen_post_types' );
+add_action( 'init', 'bcb_leadgen_post_types', -10 );
+
+
+/**
+ * Adds our custom lead_cat rewrite rules.
+ *
+ * @param  array $lead_cat_rewrite lead_cat rewrite rules.
+ *
+ * @return array
+ */
+function no_lead_cat_base_rewrite_rules( $lead_cat_rewrite = array() ) {
+    global $wp_rewrite;
+    
+	$lead_cat_rewrite = array();
+
+	$lead_cats = get_terms( array( 'taxonomy' => 'lead_cat', 'hide_empty' => false ) );
+
+	foreach( $lead_cats as $lead_cat ) {
+		$lead_cat_nicename = $lead_cat->slug;
+
+		if ( $lead_cat->parent == $lead_cat->cat_ID ) {
+			$lead_cat->parent = 0;
+		} elseif ( $lead_cat->parent != 0 ) {
+            $lead_cat_nicename = get_term_parents_list( $lead_cat->parent, 'lead_cat', array( 
+                'link' => false,
+                'format' => 'slug',
+            ) );
+		}
+
+		$lead_cat_rewrite['('.$lead_cat_nicename.')/(?:feed/)?(feed|rdf|rss|rss2|atom)/?$'] = 'index.php?lead_cat=$matches[1]&feed=$matches[2]';
+		$lead_cat_rewrite["({$lead_cat_nicename})/{$wp_rewrite->pagination_base}/?([0-9]{1,})/?$"] = 'index.php?lead_cat=$matches[1]&paged=$matches[2]';
+        $lead_cat_rewrite['('.$lead_cat_nicename.')/?$'] = 'index.php?lead_cat=$matches[1]';
+        $lead_cat_rewrite['('.$lead_cat_nicename.')/([^/]+)(?:/([0-9]+))?/?$'] = 'index.php?lead_cat=$matches[1]&leadpage=$matches[2]&page=$matches[3]';
+	}
+
+	return $lead_cat_rewrite;
+}
+add_filter( 'lead_cat_rewrite_rules', 'no_lead_cat_base_rewrite_rules' );
+
+add_action( 'created_lead_cat',  'flush_rewrite_rules' );
+add_action( 'delete_lead_cat',   'flush_rewrite_rules' );
+add_action( 'edited_lead_cat',   'flush_rewrite_rules' );
